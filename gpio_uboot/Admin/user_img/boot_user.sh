bd7ee5697c36148e743bf2d6bdfbd9b6b8f14941
Paweł Grabowski <p.grabowski2@student.mini.pw.edu.pl>
2020-04-19 23:39:27 +0200

    deleted unnecessary files

--- gpio_uboot/Admin/user_img/boot_user.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-(
-  export PATH=../buildroot-2020.02/output/host/bin:../../Utility/buildroot-2020.02/output/host/bin:$PATH
-  mkimage -T script -C none -n 'Start script' -d boot_user.txt boot.scr
-)
